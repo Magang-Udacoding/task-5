82face12f26ca2c9007493548311ac5300a0fba3
FailHy <hylmikhalil@gmail.com>
2026-09-02 10:56:21 +0700

    Feat: penambahan projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,10 +2,10 @@ import { useEffect } from "react";
 import ProjectCard from "../components/ProjectCard";
 
 function Projects() {
-    // useEffect 
-    useEffect (()=> {
-      document.title = 'Portfolio | Projects'
-    }, [])
+  // useEffect
+  useEffect(() => {
+    document.title = "Portfolio | Projects";
+  }, []);
 
   const projects = [
     {
@@ -39,16 +39,22 @@ function Projects() {
       desc: "Personal portfolio multi-page dibangun dengan React 18, React Router v6, dan Tailwind CSS v4.",
       tech: ["React", "Tailwind CSS", "Vite", "React Router"],
       image: "https://placehold.co/600x400/1e293b/60a5fa?text=Portfolio",
-      link: "https://github.com/Magang-Udacoding/task-5",
+      link: "https://https://aboutpail.vercel.app",
+    },
+    {
+      id: 5,
+      title: "Money Tracker Website",
+      desc: "Aplikasi pelacakan pengeluaran dan pemasukan keuangan secara native.",
+      tech: ["React", "Tailwind CSS", "Vite", "React Router"],
+      image: "https://placehold.co/600x400/1e293b/60a5fa?text=Money+Tracker",
+      link: "https://mymoneytracxed.vercel.app",
     },
   ];
 
   return (
     <main className="min-h-screen bg-gray-50 dark:bg-gray-950 text-gray-900 dark:text-white">
-
       {/* header */}
       <section className="py-20 px-6 text-center">
-
         <h1 className="text-4xl md:text-5xl font-bold mb-4">
           My <span className="text-blue-400"> Projects</span>
         </h1>
@@ -60,23 +66,21 @@ function Projects() {
 
       {/* grid */}
       <section className="pb-20 px-6">
-
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-
-          {projects.map ((project) => (
-            <ProjectCard 
-            key = {project.id}
-            title = {project.title}
-            desc = {project.desc}
-            tech = {project.tech}
-            image = {project.image}
-            link = {project.link}
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              desc={project.desc}
+              tech={project.tech}
+              image={project.image}
+              link={project.link}
             />
           ))}
         </div>
       </section>
     </main>
-  )
+  );
 }
 
 export default Projects;
